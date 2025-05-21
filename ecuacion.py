5c04c5d100140e8a2d6d002de52fd9231dd26145
kuno969 <edgardo.cuba.alarcon.upch@gmail.com>
2025-05-20 20:34:07 -0500

    Update ecuacion.py

--- ecuacion.py
+++ ecuacion.py
@@ -19,13 +19,13 @@ def resolver_ecuacion_2do_grado(a, b, c):
         return (f"Tiene dos soluciones complejas:\n"
                 f"x1 = {parte_real} + {parte_imaginaria}i\n"
                 f"x2 = {parte_real} - {parte_imaginaria}i")
-        # Ejemplo de uso
+
+# Ejemplo de uso
 try:
     a = float(input("Ingrese el coeficiente a: "))
     b = float(input("Ingrese el coeficiente b: "))
-    resultado = resolver_ecuacion_1er_grado(a, b)
+    c = float(input("Ingrese el coeficiente c: "))
+    resultado = resolver_ecuacion_2do_grado(a, b, c)
     print(resultado)
 except ValueError:
     print("Por favor, ingrese valores numéricos válidos.")
-
-
